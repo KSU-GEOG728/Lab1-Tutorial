14ea86cd399799fb74ab5f04cb9294e69826c686
Katherine S Nelson <ksnelson@ksu.edu>
2020-01-17 17:04:41 -0600

    really final

---
+++ Intro_to_R/Starter script.R
@@ -0,0 +1,19 @@
+
+
+R.Version()
+
+citation()
+
+install.packages("sf") #sf is a fantastic package for handling spatial data that we will use often later in the semester
+
+library(sf)
+
+update.packages()
+
+?read.csv #get help info on reading in a csv file
+
+?hist
+
+??csv
+
+??histogram #search for help topics realted to histograms
